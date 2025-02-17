0d7c14d69a2de4feb608072368b145205c34dea5
Baachoven <AK.lumina.inc@gmail.com>
2025-02-17 21:49:22 +0900

    fix: middleware error

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,30 +5,6 @@ import { supabase } from './utils/supabase/supabase'
 export async function middleware(request: NextRequest) {
   const res = NextResponse.next()
 
-  // セッション変更を監視
-  const { data } = supabase.auth.onAuthStateChange((event, session) => {
-    console.log(event, session)
-
-    if (event === 'INITIAL_SESSION') {
-      // handle initial session
-    } else if (event === 'SIGNED_IN') {
-      // handle sign in event
-      return NextResponse.redirect(new URL('/user', request.url))
-    } else if (event === 'SIGNED_OUT') {
-      // handle sign out event
-    } else if (event === 'PASSWORD_RECOVERY') {
-      // handle password recovery event
-    } else if (event === 'TOKEN_REFRESHED') {
-      // handle token refreshed event
-    } else if (event === 'USER_UPDATED') {
-      // handle user updated event
-    }
-  })
-
-  // call unsubscribe to remove the callback
-  data.subscription.unsubscribe()
-
-
   const {
     data: { session },
   } = await supabase.auth.getSession()
